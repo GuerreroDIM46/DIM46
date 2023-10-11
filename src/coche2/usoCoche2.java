3638d88f8d0a39ff2911c872587f74fa6fc614a0
Xavier Guerrero <144020944+GuerreroDIM46@users.noreply.github.com>
2023-10-11 17:56:43 +0200

    coches

--- src/coche2/usoCoche2.java
+++ src/coche2/usoCoche2.java
@@ -6,7 +6,6 @@ public class usoCoche2 {
 
 	public static void main(String[] args) {
 		coche ferrari = new coche("rojo");
-		System.out.println(ferrari.getRuedas());
 		System.out.println(ferrari.toString());
 		
 	}
